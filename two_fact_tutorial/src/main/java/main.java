341098e49b7756c0a8e971a5a0e5dea96364248a
Zach Borromeo <zachary.p.borromeo@vanderbilt.edu>
2021-03-20 12:14:48 -0500

    Updated missing libraries and moved around public classes + methods. Missing two libraries/have two errors

--- two_fact_tutorial/src/main/java/main.java
+++ two_fact_tutorial/src/main/java/main.java
@@ -1,46 +1,49 @@
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Hex;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.security.SecureRandom;
-//import Base32
 
 public class main {
+    public static void main(String args[]) {
 
+        //QDWSM3OYBPGTEVSPB5FKVDM3CSNCWHVK --> secret key
 
-    //QDWSM3OYBPGTEVSPB5FKVDM3CSNCWHVK --> secret key
+        //generic manual entry
+        String secretKey = "QDWSM3OYBPGTEVSPB5FKVDM3CSNCWHVK";
+        String lastCode = null;
 
-    //generic manual entry
-    String secretKey = "QDWSM3OYBPGTEVSPB5FKVDM3CSNCWHVK";
-    String lastCode = null;
-    while (true) {
-        String code = getTOTPCode(secretKey);
-        if (!code.equals(lastCode)) {
-            System.out.println(code);
+        while (true) {
+            String code = getTOTPCode(secretKey);
+            if (!code.equals(lastCode)) {
+                System.out.println(code);
+            }
+            lastCode = code;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {}
         }
-        lastCode = code;
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {};
-    }
 
-    //QR code stuff
-     public static String getGoogleAuthenticatorBarCode(String secretKey, String account, String issuer) {
-        try {
-            return "otpauth://totp/"
-                    + URLEncoder.encode(issuer + ":" + account, "UTF-8").replace("+", "%20")
-                    + "?secret=" + URLEncoder.encode(secretKey, "UTF-8").replace("+", "%20")
-                    + "&issuer=" + URLEncoder.encode(issuer, "UTF-8").replace("+", "%20");
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalStateException(e);
-        }
-    }
+        //test
+        String email = "test@gmail.com";
+        String companyName = "My Awesome Company";
+        String barCodeUrl = Utils.getGoogleAuthenticatorBarCode(secretKey, email, companyName);
+        System.out.println(barCodeUrl);
 
-    //test
-    String secretKey = "QDWSM3OYBPGTEVSPB5FKVDM3CSNCWHVK";
-    String email = "test@gmail.com";
-    String companyName = "My Awesome Company";
-    String barCodeUrl = Utils.getGoogleAuthenticatorBarCode(secretKey, email, companyName);
-    System.out.println(barCodeUrl);
+
+    }
 
     //generate QR Code
-    public static void createQRCode(String barCodeData, String filePath, int height, int width)
+    public static void createQRCode (String barCodeData, String filePath,int height, int width)
             throws WriterException, IOException {
         BitMatrix matrix = new MultiFormatWriter().encode(barCodeData, BarcodeFormat.QR_CODE,
                 width, height);
@@ -50,7 +53,7 @@ public class main {
     }
 
     //key generation
-    public static String generateSecretKey() {
+    public static String generateSecretKey () {
         SecureRandom random = new SecureRandom();
         byte[] bytes = new byte[20];
         random.nextBytes(bytes);
@@ -59,10 +62,23 @@ public class main {
     }
 
     //method to return key
-    public static String getTOTPCode(String secretKey) {
+    public static String getTOTPCode (String secretKey){
         Base32 base32 = new Base32();
         byte[] bytes = base32.decode(secretKey);
         String hexKey = Hex.encodeHexString(bytes);
         return TOTP.getOTP(hexKey);
     }
+
+    //QR code stuff
+    public static String getGoogleAuthenticatorBarCode (String secretKey, String account, String issuer){
+        try {
+            return "otpauth://totp/"
+                    + URLEncoder.encode(issuer + ":" + account, "UTF-8").replace("+", "%20")
+                    + "?secret=" + URLEncoder.encode(secretKey, "UTF-8").replace("+", "%20")
+                    + "&issuer=" + URLEncoder.encode(issuer, "UTF-8").replace("+", "%20");
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
+
