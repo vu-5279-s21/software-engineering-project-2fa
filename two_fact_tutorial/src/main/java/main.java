5d151d066e80dd353097151e4b97bc8d4861cfd7
Zach Borromeo <zachary.p.borromeo@vanderbilt.edu>
2021-03-21 12:35:09 -0500

    Resolved one missing library issue. Now only missing Utils Library

--- two_fact_tutorial/src/main/java/main.java
+++ two_fact_tutorial/src/main/java/main.java
@@ -4,6 +4,7 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import de.taimos.totp.TOTP;
 import org.apache.commons.codec.binary.Base32;
 import org.apache.commons.codec.binary.Hex;
 
